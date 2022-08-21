f12e7d3fc70ea8cd425ab2724d39cf4a12e4e918
LC Clark <laura.mc.clark@gmail>
2022-08-21 10:18:39 -0400

    init commit: the sequel

--- script.js
+++ script.js
@@ -1,89 +1,75 @@
-let playerX = {
-    name: "X",
-    cellsClaimed: []
-    }
-    
-    let playerO = {
-      name: "O",
-      cellsClaimed: []
-    }
-    
-    let currentPlayer = playerX;
-    
-    const allCells = document.querySelectorAll('.cell');
-    const stampSigil = function(e) {
-    e.target.innerText = currentPlayer.name;
-      
-    if (currentPlayer=== playerX)  {
-      currentPlayer = playerO;
-    }
-    else {
-      currentPlayer = playerX;
-    }  
-      toggleClass(e);
-      
-    }
-    allCells.forEach((cell) => {
-      cell.addEventListener('click', stampSigil);
-    })
-    
-    const toggleClass = function(e) {
-      if (e.target.classList.contains('.cell')) {
-        e.target.classList.remove('.cell');
+const board = document.getElementById("board");
+const messageArea = document.getElementById("message-area");
+
+const btn = document.querySelector("button");
+const playerX = { name: "X", cells: [] };
+const playerO = { name: "O", cells: [] };
+
+let c1 = document.getElementById("top-left");
+let c2 = document.getElementById("top-middle");
+let c3 = document.getElementById("top-right");
+let c4 = document.getElementById("middle-left");
+let c5 = document.getElementById("middle-middle");
+let c6 = document.getElementById("middle-right");
+let c7 = document.getElementById("bottom-left");
+let c8 = document.getElementById("bottom-middle");
+let c9 = document.getElementById("bottom-right");
+
+const allCells = [c1, c2, c3, c4, c5, c6, c7, c8, c9];
+
+const s1 = [c1, c2, c3];
+const s2 = [c4, c5, c6];
+const s3 = [c7, c8, c9];
+const s4 = [c1, c5, c9];
+const s5 = [c3, c5, c7];
+const s6 = [c1, c4, c7];
+const s7 = [c2, c5, c8];
+const s8 = [c3, c6, c9];
+const players = [playerX, playerO];
+let currentPlayer = players[0];
+const winningStreaks = [s1, s2, s3, s4, s5, s6, s7, s8];
+
+function checkStreaks() {
+  players.forEach((player) => {
+    winningStreaks.forEach((streak) => {
+      if (
+        player.cells.includes(streak[0]) &&
+        player.cells.includes(streak[1]) &&
+        player.cells.includes(streak[2])
+      ) {
+        messageArea.textContent = player.name + " WINS!";
       }
-      
-    }
-    
-    
-    
-    
-    
-    
-    /*
-    let r1 = document.querySelectorAll('.vR-1')
-    r1.forEach((cell) =>{
+    });
+  });
+}
+
+function switchTurns() {
+  if (currentPlayer === players[0]) {
+    currentPlayer = players[1];
+  } else {
+    currentPlayer = players[0];
+  }
+}
+
+function playerStamp(e) {
+  if (!e.target.classList.contains("clicked")) {
+    e.target.innerText = currentPlayer.name;
+    currentPlayer.cells.push(e.target);
+    e.target.classList.add("clicked");
+
+    checkStreaks();
+    switchTurns();
+  }
+}
+
+function reset() {
+  allCells.forEach((cell) => {
+    cell.classList.remove("clicked");
+    cell.textContent = "";
+    messageArea.textContent = "";
     console.log(cell);
-    })
-    console.log("");
-    let r2 = document.querySelectorAll('.vR-2')
-    r2.forEach((cell) =>{
-    console.log(cell);
-    })
-    console.log("");
-    let r3 = document.querySelectorAll('.vR-3')
-    r3.forEach((cell) =>{
-    console.log(cell);
-    })
-    console.log("");
-    let c1 = document.querySelectorAll('.vC-1')
-    c1.forEach((cell) =>{
-    console.log(cell);
-    })
-    console.log("");
-    let c2 = document.querySelectorAll('.vC-2')
-    c2.forEach((cell) =>{
-    console.log(cell);
-    })
-    console.log("");
-    let c3 = document.querySelectorAll('.vC-3')
-    c3.forEach((cell) =>{
-    console.log(cell);
-    })
-    console.log("");
-    let d1 = document.querySelectorAll('.vD-1')
-    d1.forEach((cell) =>{
-    console.log(cell);
-    })
-    console.log("");
-    let d2 = document.querySelectorAll('.vD-2')
-    d2.forEach((cell) =>{
-    console.log(cell);
-    })
-    
-    const allStreaks = [r1,r2,r3,c1,c2,c3,d1,d2]
-    /*
-    const clickabEls = document.querySelectorAll('.practice-div');
-    
-    clickabEls.forEach((ele) => {
-      ele.addEventListener('click', stampSigil);
-    })*/
+  });
+  console.log("reset");
+}
+board.addEventListener("click", playerStamp);
+btn.addEventListener("click", reset);
